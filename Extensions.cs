0a2d57dc478ffa6ca83f6acaf94fab8eca806e78
CodeMonkey <84345037+BillOatmanWork@users.noreply.github.com>
2022-04-16 14:45:08 -0400

    Add files via upload

---
+++ Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utube2M3u
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Extracts the substring starting from 'start' position to 'end' position.
+        /// </summary>
+        /// <param name="s">The given string.</param>
+        /// <param name="start">The start position.</param>
+        /// <param name="end">The end position.</param>
+        /// <returns>The substring.</returns>
+        public static string SubstringFromXToY(this string s, int start, int end)
+        {
+            return s.Substring(start, end - start);
+        }
+    }
+}
---
+++ Utube2M3u.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utube2M3u
+{
+    class Utube2M3u
+    {
+        static void Main(string[] args)
+        {
+            Utube2M3u p = new Utube2M3u();
+            p.RealMain(args);
+        }
+
+        private void RealMain(string[] args)
+        {
+            Console.WriteLine("Utube2M3u version " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("");
+
+            if (args.Length != 2)
+            {
+                DisplayHelp();
+                return;
+            }
+
+            string inFile = args[0];
+            string outFile = args[1];
+
+            Console.WriteLine($"Input file: {inFile}   Output file: {outFile}");
+            Console.WriteLine("");
+
+            StringBuilder m3uLines = new StringBuilder();
+            string lineSave = string.Empty;
+
+            try
+            {
+                foreach (string line in System.IO.File.ReadLines(inFile))
+                {
+                    string[] parts = line.Split('|');
+
+                    if (parts.Length != 5)
+                    {
+                        Console.WriteLine("Line format incorrect.");
+                        Console.WriteLine(line);
+                        Console.WriteLine("");
+
+                        continue;
+                    }
+
+                    lineSave = line;
+
+                    string content;
+                    using (var client = new HttpClient())
+                    {
+                        content = client.GetStringAsync(parts[4]).Result;
+                    }
+
+                    m3uLines.Append("#EXTINF:-1");
+
+                    if(!string.IsNullOrEmpty(parts[1]))
+                        m3uLines.Append($" CUID=\"{parts[1]}\"");
+
+                    m3uLines.Append(" tvg-id=\"\" tvg-name=\"\"");
+
+                    if (!string.IsNullOrEmpty(parts[3]))
+                        m3uLines.Append($" tvg-logo=\"{parts[3]}\"");
+
+                    m3uLines.AppendLine($" group-title=\"{parts[2]}\",{parts[0]}");
+
+                    int endIndex = content.IndexOf(".m3u8", 0);
+                    int startIndex = content.LastIndexOf("https://", endIndex);
+                    string url = content.SubstringFromXToY(startIndex, endIndex + 5);
+                    m3uLines.AppendLine(url);
+                }
+
+                using (TextWriter tw = new StreamWriter(outFile, false))
+                {
+                    tw.WriteLine(m3uLines.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}.");
+                Console.WriteLine(lineSave);
+                Console.WriteLine("");
+            }
+        }
+
+        private void DisplayHelp()
+        {
+            Console.WriteLine("Parameters: (Case Insensitive)");
+            Console.WriteLine("\tUser = Jarvis user name.");
+            Console.WriteLine("\tPass = Jarvis password.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Hit enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
+
