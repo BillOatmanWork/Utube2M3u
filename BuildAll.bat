b3f263d305c1a045deafa6e21062c2e7d7d47c65
Bill Oatman <woatman@rochester.rr.com>
2022-05-01 13:55:07 -0400

    Clean up sample bat file

--- BuildAll.bat
+++ BuildAll.bat
@@ -7,28 +7,3 @@ dotnet publish -r ubuntu.18.04-x64 -c Release /p:PublishSingleFile=true /p:Inclu
 dotnet publish -r alpine.3.12-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 
-cd test
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\win-x64\publish\J3u.exe" .
-"C:\Program Files\7-Zip\7z" a -tzip J3u-WIN.zip J3u.exe
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\debian.10-x64\publish\J3u" .
-"C:\Program Files\7-Zip\7z" a -t7z J3u-DEB10.7z J3u
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\debian-x64\publish\J3u" .
-"C:\Program Files\7-Zip\7z" a -t7z J3u-DEB9.7z J3u
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\linux-arm\publish\J3u" .
-"C:\Program Files\7-Zip\7z" a -t7z J3u-RasPi.7z J3u
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\osx.10.14-x64\publish\J3u" .
-"C:\Program Files\7-Zip\7z" a -t7z J3u-OSX.7z J3u
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\ubuntu.18.04-x64\publish\J3u" .
-"C:\Program Files\7-Zip\7z" a -t7z J3u-UBU.7z J3u
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\alpine.3.12-x64\publish\J3u" .
-"C:\Program Files\7-Zip\7z" a -t7z J3u-ALP.7z J3u
-
-copy /Y "C:\Stuff\J3u\J3u\bin\Release\net5.0\linux-x64\publish\J3u" .
-"C:\Program Files\7-Zip\7z" a -t7z J3u-LIN64.7z J3u
